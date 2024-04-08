e859cdb3c4f353076a5a39601d6260b44d721d0b
emma673r <90177139+emma673r@users.noreply.github.com>
2024-04-08 11:25:17 +0200

    And so it begins..

---
+++ src/main/java/com/example/dreamcloud/DreamCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.dreamcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dreamcloud/DreamCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
