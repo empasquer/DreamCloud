f6099bf916f93f32945b281165c0eba2605b0f08
emma673r <90177139+emma673r@users.noreply.github.com>
2024-04-11 09:57:39 +0200

    models made with byte for profile picture

---
+++ src/main/java/com/example/dreamcloud/model/Wishlist.java
@@ -0,0 +1,55 @@
+package com.example.dreamcloud.model;
+
+import java.util.ArrayList;
+
+public class Wishlist {
+
+    private int wishlistId;
+    private String wishlistTitle;
+    private String wishlistDescription;
+    private String profileUsername;
+    private ArrayList<Wish> wishes;
+
+    public Wishlist() {
+    }
+
+    public int getWishlistId() {
+        return wishlistId;
+    }
+
+    public String getWishlistTitle() {
+        return wishlistTitle;
+    }
+
+    public String getWishlistDescription() {
+        return wishlistDescription;
+    }
+
+    public String getProfileUsername() {
+        return profileUsername;
+    }
+
+    public ArrayList<Wish> getWishes() {
+        return wishes;
+    }
+
+    public void setWishlistId(int wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public void setWishlistTitle(String wishlistTitle) {
+        this.wishlistTitle = wishlistTitle;
+    }
+
+    public void setWishlistDescription(String wishlistDescription) {
+        this.wishlistDescription = wishlistDescription;
+    }
+
+    public void setProfileUsername(String profileUsername) {
+        this.profileUsername = profileUsername;
+    }
+
+    public void setWishes(ArrayList<Wish> wishes) {
+        this.wishes = wishes;
+    }
+}
